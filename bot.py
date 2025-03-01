86b6f4c3d3c789776985f47411553eb8efb58f03
2022fw <111440550+2022fw@users.noreply.github.com>
2025-03-01 15:36:37 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,10 +7,10 @@ import logging
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 SLACK_CHANNEL = os.getenv("SLACK_CHANNEL", "#나스닥")  # 채널을 #나스닥으로 설정
 
-def get_stock_price(ticker="^IXIC"):  # 나스닥 지수의 티커는 ^IXIC
+def get_stock_price(ticker="^IXIC", name):  # 나스닥 지수의 티커는 ^IXIC
     stock = yf.Ticker(ticker)
     price = stock.history(period="1d")["Close"].iloc[-1]
-    return f"나스닥 지수 참고하렴 *{price:.2f}*  :rocket:"
+    return f"{name} 미국 경제 지수 참고하렴 *{price:.2f}*  :rocket:"
 
 def send_slack_message(text):
     url = "https://slack.com/api/chat.postMessage"
@@ -32,9 +32,13 @@ def is_market_open():
 if __name__ == "__main__":
     if is_market_open():
         print('market open')
-        message = get_stock_price("^IXIC")  # 나스닥 지수 가격 가져오기
+        message1 = get_stock_price("^IXIC")  # 나스닥 지수 가격 가져오기
+        message2 = get_stock_price("^GSPC")  # 슨피 지수 가격 가져오기
+        
         print(message)
-        result = send_slack_message(message)
+        result = send_slack_message(message1, '나스닥')
+        send_slack_message(message2, 's&p500')
+        
         print(result)
     else:
         send_slack_message("미국 시장은 현재 열려 있지 않습니다.")
