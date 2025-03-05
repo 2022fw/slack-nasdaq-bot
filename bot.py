fb2a61c767a175890f5a19601cd433fd7fb8267a
crayola1331 <36468973+crayola1331@users.noreply.github.com>
2025-03-05 13:14:46 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -34,7 +34,7 @@ def get_stock_price(ticker="^IXIC", name="나스닥"):  # 나스닥 지수의 �
 def get_bitcoin_price():
     try:
         bitcoin = yf.Ticker("BTC-USD")
-        logging.error(f"여기 {bitcoin}")
+        logging.error(f"여기 {bitcoin.info}")
         current_price = bitcoin.info['currentPrice']
         return f"비트코인 가격: *{current_price}* USD"
     except Exception as e:
